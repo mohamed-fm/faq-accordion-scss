5819205d40fe7a7dd870578eef0218f43fce2cdf
Mohamed Ragheb <141369494+mohamed-fm@users.noreply.github.com>
2024-11-28 09:55:29 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,32 +4,15 @@
 
 const expand_buttons = document.querySelectorAll('.expand-buttons')
 
-function removeClassFromAllButtons(className){
-    expand_buttons.forEach((el)=>{
-        el.classList.remove(className)
-    })
-}
-function addClassToAllButtons(className){
-    expand_buttons.forEach((el)=>{
-        el.classList.add(className)
-    })
-}
-
 function setActiveButtonAndActiveBody(e){
+    const item = e.target
     expand_buttons.forEach((el)=>{
-        el.parentElement.parentElement.lastElementChild.classList.remove("active")
+        el.classList.add('plus')
+        el.classList.remove('minus')
+        el.parentElement.parentElement.lastElementChild.classList.remove('active')
     })
-    if(e.target.classList.contains('plus')){
-        removeClassFromAllButtons('minus')
-        addClassToAllButtons('plus')
-        e.target.classList.add("minus")
-        e.target.classList.remove('plus')
-        e.target.parentElement.parentElement.lastElementChild.classList.add("active")
-    }else{
-        e.target.classList.remove('minus')
-        e.target.classList.add('plus')
-        e.target.parentElement.parentElement.lastElementChild.classList.remove("active")
-    }
+    item.classList.add('minus')
+    item.parentElement.parentElement.lastElementChild.classList.add('active')
 }
 expand_buttons.forEach((el)=>{
     el.addEventListener("click", setActiveButtonAndActiveBody )
